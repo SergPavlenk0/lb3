6563c0490a59789397885b4d556533d15e540273
Sergey Pavlenko <sergeipavlenko2020@gmail.com>
2021-10-23 21:06:01 +0300

    lb3 completed

---
+++ OOP_LB3/OOP_LB3/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_LB3
+{
+    public class Calculator
+    {
+        double x, y;
+
+        public Calculator(double X, double Y, char operation)
+        {
+            x = X;
+            y = Y;
+
+            if (operation == '+') Addition();
+            else if (operation == '-') Deduction();
+            else if (operation == '*') Multiplication();
+        }
+
+        public void Addition()
+        {
+            Console.WriteLine(x + y);
+        }
+        public void Deduction()
+        {
+            Console.WriteLine(x - y);
+        }
+        public void Multiplication()
+        {
+            Console.WriteLine(x * y);
+        }
+    }
+}
--- OOP_LB3/OOP_LB3/Program.cs
+++ OOP_LB3/OOP_LB3/Program.cs
@@ -4,33 +4,7 @@ using System.Linq;
 
 namespace Lab_3
 {
-    public class Calculator
-    {
-        double x, y;    
-
-        public Calculator(double X, double Y, char operation)
-        {
-            x = X;
-            y = Y;
-
-            if (operation == '+') Addition();
-            else if (operation == '-') Deduction();
-            else if (operation == '*') Multiplication();
-        }
-
-        public void Addition()
-        {
-            Console.WriteLine(x + y);
-        }
-        public void Deduction()
-        {
-            Console.WriteLine(x - y);
-        }
-        public void Multiplication()
-        {
-            Console.WriteLine(x * y);
-        }
-    }
+    
     class Program
     {
         static void Main(string[] args)
